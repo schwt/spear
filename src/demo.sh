906f964efdc3620b750058e371f47f5770be6d94
schwt <cosmy9@163.com>
2017-09-13 12:01:17 +0800

    update demo.sh

--- src/demo.sh
+++ src/demo.sh
@@ -8,18 +8,23 @@
 dir=$(cd $(dirname $0); pwd)
 
 # build
+echo "\n\nbuilding ......"
 cd $dir/../lib/
 make clean; make
 cd $dir/
 make clean; make
 
 # generate random training set
+echo "\n\nmake data ......"
+cd $dir/../data/
 ./generate_data.py 1000000
 
-
 # run train
+echo "\n\ntraining ......"
+cd $dir
 ./spear config.ini
 
+echo "done."
 
 # output files are in ../data/
 cd $dir/../data/
